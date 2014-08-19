70414f2f4a02a2d10b03e0b329f9b5c1f9d4b704
Bosko Devetak <bosko.devetak@gmail.com>
2014-08-19 14:54:03 +0200

    Bump version to 0.011. Correction to comment for get function.

--- lib/HBase/JSONRest.pm
+++ lib/HBase/JSONRest.pm
@@ -11,7 +11,7 @@ use JSON::XS qw(decode_json encode_json);
 use Time::HiRes qw(gettimeofday time);
 use Data::Dumper;
 
-our $VERSION = "0.010";
+our $VERSION = "0.011";
 
 my %INFO_ROUTES = (
     version => '/version',
@@ -117,12 +117,14 @@ sub version {
 # get
 #
 # usage:
-#   my ($records, $err) = $hbase->get(
-#        table   => 'table_name',
-#        where   => {
-#            key_begins_with => "key_prefix"
-#        },
-#    );
+#
+# my $records = $hbase->get({
+#    table       => $table_name,
+#    where       => {
+#        key_begins_with => "$key_prefix"
+#    },
+# });
+#
 sub get {
     my $self   = shift;
     my $params = shift;
@@ -616,7 +618,7 @@ Information on error is stored in hbase object under key last error:
 
 =head1 VERSION
 
-Current version: 0.010
+Current version: 0.011
 
 =head1 AUTHOR
 
