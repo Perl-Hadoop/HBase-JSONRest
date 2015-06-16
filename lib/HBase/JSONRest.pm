733e904f8d72146a3c650cea5a6565f562948107
Bosko Devetak <bosko.devetak@gmail.com>
2015-06-16 11:55:49 +0200

    Bumped version to 0.031

--- lib/HBase/JSONRest.pm
+++ lib/HBase/JSONRest.pm
@@ -11,7 +11,7 @@ use JSON::XS qw(decode_json encode_json);
 use Time::HiRes qw(gettimeofday time);
 use Data::Dumper;
 
-our $VERSION = "0.030";
+our $VERSION = "0.031";
 
 my %INFO_ROUTES = (
     version => '/version',
@@ -869,7 +869,7 @@ Information on error is stored in hbase object under key last error:
 
 =head1 VERSION
 
-Current version: 0.021
+Current version: 0.031
 
 =head1 AUTHOR
 
